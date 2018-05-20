5f4699c636567dbbdeee6f05288331f2bfd398ad
neha <nsharma215@gmail.com>
2018-05-20 16:25:50 +0530

    Added the EUR method to handle the base calculations

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 export default currencyConventor = (price, baseCurrency, targetCurrency) => {
     axios.get('http://data.fixer.io/api/latest?access_key=5cd55a2f9de07d559e48ee9cbdf4e6b1')
     .then(function(response){
-        if(targetCurrency === 'EUR'){
+        const currentRateList = response.data.rates;
 
+        baseCurrency = baseCurrency.toUpperCase();
+        targetCurrency = targetCurrency.toUpperCase();
+
+        const targetCurrencyRate = (response.data.rates[targetCurrency]).toFixed(2);
+        const currentRate_BASEEUR = (response.data.rates[baseCurrency]).toFixed(2);
+        console.log(response.data.rates[baseCurrency]);
+       
+
+        if(baseCurrency === 'EUR'){
+            const value = (price * targetCurrencyRate).toFixed(2);
+            return value;
         }
         else{
         
-        const currentRateList = response.data.rates;
-        const currentRate_BASEEUR = (response.data.rates[baseCurrency]).toFixed(2);
-        const targetCurrencyRate = (response.data.rates[targetCurrency]).toFixed(2);
         const value = (price/currentRate_BASEEUR * targetCurrencyRate).toFixed(2);
-        
         return value;
         }
        
     })
     .catch(function(err){
-        console.log(err);
+        throw err;
     })
 
 }
