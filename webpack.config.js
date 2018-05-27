e424a084fa4f3324389d6759245b80af69d1fc29
neha <nsharma215@gmail.com>
2018-05-27 22:58:18 +0530

    small changes done

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,9 @@ var isProduction = process.env.NODE_ENV === "production" ? true : false;
 console.log("Build is production : " + isProduction)
 
 var config = {
-    entry: './lib/index.js',
+    entry: ["./lib/index.js", "./example/sample.js"],
     output: {
-        filename: './index.js',
-        libraryTarget : "var"
+        filename: './index.js'
     },
     mode : "development",
     module: {
@@ -27,6 +26,7 @@ var config = {
 
 if(isProduction)
     {
+        config.entry = './lib/index.js',
         config.output.libraryTarget = 'commonjs2';
         config.mode = "production";
     }
