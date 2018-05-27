d34128cc09f89099d8719b36c52a1d21f8bddab6
neha <nsharma215@gmail.com>
2018-05-27 22:14:36 +0530

    Changes in index.js and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,34 @@
 var webpack = require('webpack');
+var isProduction = process.env.NODE_ENV === "production" ? true : false;
 
-module.exports = {
-	entry : './lib/index.js',
-	output : {
-		filename : './index.js'
-	},
-	module: {
-		rules: [
-		  {
-			test: /\.js$/,
-			exclude: /(node_modules|bower_components)/,
-			use: {
-			  loader: 'babel-loader',
-			  options: {
-				presets: ['@babel/preset-env']
-			  }
-			}
-		  }
-		]
-	  }
-	
+console.log("Build is production : " + isProduction)
+
+var config = {
+    entry: './lib/index.js',
+    output: {
+        filename: './index.js',
+        libraryTarget : "var"
+    },
+    mode : "development",
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        }]
+    }
+
 };
+
+if(isProduction)
+    {
+        config.output.libraryTarget = 'commonjs2';
+        config.mode = "production";
+    }
+
+module.exports = config;
